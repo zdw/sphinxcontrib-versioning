d284a88c5cb48cdec7c0a9934f99ba22aac94dba
Taylor J. Meek <Lingnik@users.noreply.github.com>
2017-09-22 11:52:04 -0700

    remove unused var; comment on backwards compat
    Fixing linter error.

--- sphinxcontrib/versioning/sphinx_.py
+++ sphinxcontrib/versioning/sphinx_.py
@@ -57,9 +57,11 @@ class EventHandlers(object):
 
         # Add versions.html to sidebar.
         if '**' not in app.config.html_sidebars:
+            # default_sidebars was deprecated in Sphinx 1.6+, so only use it if possible (to maintain
+            # backwards compatibility), else don't use it.
             try:
                 app.config.html_sidebars['**'] = StandaloneHTMLBuilder.default_sidebars + ['versions.html']
-            except AttributeError as e:
+            except AttributeError:
                 app.config.html_sidebars['**'] = ['versions.html']
         elif 'versions.html' not in app.config.html_sidebars['**']:
             app.config.html_sidebars['**'].append('versions.html')
