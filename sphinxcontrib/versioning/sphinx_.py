876e89a47188fac80dbf1f63e49e880a2b48ea1e
Taylor J. Meek <Lingnik@users.noreply.github.com>
2017-09-22 09:51:18 -0700

    backwards compatibility for default_sidebars
    Tests appeared to start failing with my change. Realized that some test or users may be relying on the pre-1.6 default_sidebars behavior, threw in an EAFP.

--- sphinxcontrib/versioning/sphinx_.py
+++ sphinxcontrib/versioning/sphinx_.py
@@ -57,7 +57,10 @@ class EventHandlers(object):
 
         # Add versions.html to sidebar.
         if '**' not in app.config.html_sidebars:
-            app.config.html_sidebars['**'] = ['versions.html']
+            try:
+                app.config.html_sidebars['**'] = StandaloneHTMLBuilder.default_sidebars + ['versions.html']
+            except AttributeError as e:
+                app.config.html_sidebars['**'] = ['versions.html']
         elif 'versions.html' not in app.config.html_sidebars['**']:
             app.config.html_sidebars['**'].append('versions.html')
 
